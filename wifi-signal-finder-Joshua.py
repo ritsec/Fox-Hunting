11a1ce3527af045dd7d17777d061f48a6cc0380a
Joshua Niemann <jjn1155@rit.edu>
2018-09-26 21:34:37 -0400

    Fixed a few things, hot/cold is still broken

--- wifi-signal-finder-Joshua.py
+++ wifi-signal-finder-Joshua.py
@@ -1,5 +1,12 @@
 from scapy.all import *
 
+lastdb = -100
+
+def setLastdb(db):
+    lastdb = db
+
+def getLastdb():
+    return lastdb
 
 def PacketHandler(pkt) :
   if pkt.haslayer(Dot11) :
@@ -10,8 +17,15 @@ def PacketHandler(pkt) :
             rssi = -(256-ord(extra[-4:-3]))
         except:
             rssi = -1000
-            printf("Error recieved obtaining signal strength for " + pkt.addr2)
-        if(pkt.addr2.find("34:fc:b9:09:e0:80") != -1):
-            print "WiFi signal strength:", rssi, "dBm of", pkt.addr2, pkt.info
-
+            print("Error recieved obtaining signal strength for " + pkt.addr2)
+        if(pkt.addr2.find("ec:08:6b:bf:30:5a") != -1):
+        ##if(1==1):
+            print("WiFi signal strength:", rssi, "dBm of", pkt.addr2, pkt.info)
+            if(getLastdb() > rssi):
+                print("hot")
+            elif(getLastdb() == rssi):
+                print("no change")
+            else:
+                print("cold + " + str(rssi) + " " + str(getLastdb()))
+            setLastdb(rssi)
 sniff(iface="wlp0s20f0u1mon", prn = PacketHandler)
