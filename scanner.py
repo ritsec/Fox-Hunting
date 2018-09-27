8b2332bdcfbb0d1ab768bc67524ce50eec28f91a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-09-26 21:22:52 -0400

    Adhere to PIP guidelines
    Signed-off-by: Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>

--- scanner.py
+++ scanner.py
@@ -2,9 +2,11 @@ import os
 import logging
 from scapy.all import *
  
+
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
         if pkt.type == 0 and pkt.subtype == 8:
-            print("Available SSID: %s" % (pkt.info))
+            print('Available SSID: %s' % (pkt.info))
  
-sniff(iface ="mon0", prn =PacketHandler)
+
+sniff(iface='mon0', prn=PacketHandler)
