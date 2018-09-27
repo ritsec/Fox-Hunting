9d7d1cf3ad7e3e2f62776ea6c5d8a2e49e17c25f
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-09-27 16:34:42 -0400

    Add variable updating for remaining args

--- wifi-signal-finder-Joshua.py
+++ wifi-signal-finder-Joshua.py
@@ -2,12 +2,6 @@ from scapy.all import *
 import argparse
 
 
-lastdb = -100
-interface = 'wlan0mon'
-access_point = '34:fc:b9:0f:58:c0'
-
-
-
 def setLastdb(db):
     global lastdb
     lastdb = db
@@ -30,7 +24,6 @@ def PacketHandler(pkt):
                     rssi = -1000
                     print("Error recieved obtaining signal strength for ", pkt.addr2)
                 if(pkt.addr2.find(access_point) != -1):
-                ##if(1==1):
                     print("WiFi signal strength:", rssi, "dBm for", pkt.addr2 + ",", pkt.info.decode())
                     if(getLastdb() < rssi):
                         print("Hotter. Current:", str(rssi), "Last:", str(getLastdb()))
@@ -40,12 +33,19 @@ def PacketHandler(pkt):
                         print("Colder. Current:", str(rssi), "Last:", str(getLastdb()))
                     setLastdb(rssi)
 
+
 def main():
+    global lastdbm, interface, access_point
+    lastdb, interface, access_point = -100, 'wlan0mon', '34:fc:b9:0f:58:c0'
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--interface', help='interface to use')
     parser.add_argument('-a', '--accesspoint', help='access point to track')
     parser.add_argument('-v', '--values', help='display default values for the interface and access point', action='store_true')
     args = parser.parse_args()
+    if args.interface:
+        interface = args.interface
+    if args.accesspoint:
+        access_point = args.accesspoint
     if args.values:
         print('Interface: %s\nAccess Point: %s' % (interface, access_point))
     sniff(iface=transport, prn = PacketHandler)
