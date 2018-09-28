8edd26af2a4b2c39cc8f78dc0d389e1503812d53
ChrCrypto <39858259+ChrCrypto@users.noreply.github.com>
2018-09-28 10:40:00 -0400

    Misc small changes

--- active-signal-finder-Chris.py
+++ active-signal-finder-Chris.py
@@ -17,19 +17,17 @@ def getLastdb():
 
 
 def PacketHandler(pkt):
-  if pkt.haslayer(Dot11):
-    if pkt.type == 0 and pkt.subtype == 8:
-      if pkt.addr3 == sys.argv[2]:
-        rssi = -(256-ord(pkt.notdecoded[-4:-3]))
-        message = ""
-        message += str(rssi-getLastdb()) + " dBm "
-        if rssi > getLastdb():
-          message += "hotter. "
-        elif rssi <= getLastdb():
-          message += "colder. "
-        setLastdb(rssi)
-        message += (pkt.info.decode() + ": " + pkt.addr3 + ", dBm " + str(rssi))
-        print(message)
+  if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 8:
+    if pkt.addr3 == sys.argv[2]:
+      rssi = -(256-ord(pkt.notdecoded[-4:-3]))
+      message = str(rssi-getLastdb()) + " dBm "
+      if rssi > getLastdb():
+        message += "hotter. "
+      elif rssi <= getLastdb():
+        message += "colder. "
+      setLastdb(rssi)
+      message += (pkt.info.decode() + ": " + pkt.addr3 + ", dBm " + str(rssi))
+      print(message)
 
 def broadcast():
   probePacket = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="00:11:22:33:44:55",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID="SSID", info="")
@@ -44,4 +42,4 @@ if len(sys.argv)==3:
   
   sniff(iface=sys.argv[1], prn = PacketHandler)
 else:
-  print("Usage: python3 active-signal-finder-Chris.py <interface> <target bssid>")
+  print("Usage: python3 active-signal-finder-Chris.py <interface> <target BSSID>")
