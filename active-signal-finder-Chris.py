7d8963aed3012e6f0e7b211ad22917202285c97a
Chris Cheney <39858259+ChrCrypto@users.noreply.github.com>
2018-10-05 11:58:08 -0400

    Change to checking packets, disabled storing packets

--- active-signal-finder-Chris.py
+++ active-signal-finder-Chris.py
@@ -18,17 +18,18 @@ def getLastdb():
 
 def PacketHandler(pkt):
   #print(pkt.summary())
-  if pkt.haslayer(Dot11) and (pkt.type == 0 or pkt.type == "Management" ) and pkt.subtype == 8:
-    if pkt.addr3 == sys.argv[2]:
-      rssi = -(256-ord(pkt.notdecoded[-4:-3]))
-      message = str(rssi-getLastdb()) + " dBm "
-      if rssi > getLastdb():
-        message += "hotter. "
-      elif rssi <= getLastdb():
-        message += "colder. "
-      setLastdb(rssi)
-      message += (pkt.info.decode() + ": " + pkt.addr3 + ", dBm " + str(rssi))
-      print(message)
+  if pkt.haslayer(Dot11):
+    if pkt.type == 0 and pkt.subtype == 8:
+     if pkt.addr3 == sys.argv[2]:
+        rssi = -(256-ord(pkt.notdecoded[-4:-3]))
+        message = str(rssi-getLastdb()) + " dBm "
+        if rssi > getLastdb():
+          message += "hotter. "
+        elif rssi <= getLastdb():
+          message += "colder. "
+        setLastdb(rssi)
+        message += (pkt.info.decode() + ": " + pkt.addr3 + ", dBm " + str(rssi))
+        print(message)
 
 def broadcast():
   probePacket = RadioTap()/Dot11(type=0,subtype=4,addr1="ff:ff:ff:ff:ff:ff", addr2="00:11:22:33:44:55",addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID="SSID", info="")
@@ -45,7 +46,7 @@ def main():
   broadcastThread = threading.Thread(target=broadcast)
   broadcastThread.start()
   
-  sniff(iface=sys.argv[1], prn = PacketHandler)
+  sniff(iface=sys.argv[1], prn = PacketHandler, store=0)
     
 if __name__ == "__main__":
     main()
