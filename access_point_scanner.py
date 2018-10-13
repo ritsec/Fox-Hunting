e7563c9a5d31cd1f3290d489d8d6709a8011a14b
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-10-13 12:11:59 -0400

    Add hot/cold functionality

--- access_point_scanner.py
+++ access_point_scanner.py
@@ -2,9 +2,21 @@ from access_points import get_scanner
 
 
 def main():
+    tracked_bssid = 'a8:bd:27:8f:01:53'
+    last_quality = 0;
     wifi_scanner = get_scanner()
-    for ap in wifi_scanner.get_access_points():
-        print(ap)
+    print('All Access Points:\n%s' % wifi_scanner.get_access_points())
+    while True:
+        for ap in wifi_scanner.get_access_points():
+            if ap.bssid == tracked_bssid:
+                if (last_quality < ap.quality):
+                    print('Hotter')
+                elif (last_quality > ap.quality):
+                    print('Colder')
+                else:
+                    print('Same')
+                last_quality = ap.quality
+
 
 
 if __name__ == '__main__':
