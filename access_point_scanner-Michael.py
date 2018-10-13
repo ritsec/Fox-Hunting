463888f0dd074c40259461fd078c098548026bf0
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-10-13 12:25:01 -0400

    Improve output

--- access_point_scanner-Michael.py
+++ access_point_scanner-Michael.py
@@ -7,19 +7,20 @@ def scan(tracked_bssid):
     last_quality = 0;
     wifi_scanner = get_scanner()
     print('All Access Points:\n%s' % wifi_scanner.get_access_points())
+    print('Tracking %s' % tracked_bssid)
     try:
         while True:
             for ap in wifi_scanner.get_access_points():
                 if ap.bssid == tracked_bssid:
                     if (last_quality < ap.quality):
-                        print('Hotter')
+                        print('Hotter | %s' % ap)
                     elif (last_quality > ap.quality):
-                        print('Colder')
+                        print('Colder | %s' % ap)
                     else:
-                        print('Same')
+                        print('Same | %s' % ap)
                     last_quality = ap.quality
     except KeyboardInterrupt:
-        print('\nHalting Scanning')
+        print('Halting Scanning')
 
 def main():
     parser = argparse.ArgumentParser()
