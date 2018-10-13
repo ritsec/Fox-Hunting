bef797656eb705da7b146b7944cebb89e56e988a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-10-13 14:08:04 -0400

    Add -s argument

--- access_point_tracker.py
+++ access_point_tracker.py
@@ -3,7 +3,7 @@ import datetime
 from access_points import get_scanner
 
 
-def scan(all, clear, tracked_bssid):
+def scan(all, clear, same, tracked_bssid):
     last_quality, message = 0, ''
     wifi_scanner = get_scanner()
     print('All Access Points:\n%s\n' % wifi_scanner.get_access_points() if all else '', end='')
@@ -17,8 +17,10 @@ def scan(all, clear, tracked_bssid):
                     elif (last_quality > ap.quality):
                         message = 'Colder'
                     else:
+                        if same:
+                            break
                         message = 'Same  '
-                    print('%s%s (%02d) | %s @ %s' % ('\r' if clear else '', message, (ap.quality - last_quality), ap, datetime.datetime.now().time()), end=('' if clear else '\n'))
+                    print('%s%s (%02d) | %s @ %s' % ('\r' if clear else '', message, (ap.quality - last_quality),ap, datetime.datetime.now().time()), end=('' if clear else '\n'))
                     last_quality = ap.quality
     except KeyboardInterrupt:
         print('Halting Scanning')
@@ -29,8 +31,9 @@ def main():
     parser.add_argument('bssid', help='the bssid to track', type=str)
     parser.add_argument('-a', '--all', help='print all available access points', action='store_true')
     parser.add_argument('-c', '--clear', help='clear tracking line on each update', action='store_true')
+    parser.add_argument('-s', '--same', help='hide same updates', action='store_true')
     args = parser.parse_args()
-    scan(args.all, args.clear, args.bssid)
+    scan(args.all, args.clear, args.same, args.bssid)
 
 
 if __name__ == '__main__':
